9e160b0a7871bd6ad4f257b8a30b659ac45d0dbe
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-12 21:13:29 +0530

    terraform

---
+++ variables/ec2.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "terraform"{
+    ami = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = var.tags
+}
+
+resource "aws_security_group" "allow_ssh_terraform"{
+    name = var.sg_name #allow_ssh is already there in my account
+    description = var.sg_description
+    
+    #usually we allow everything in egress
+    #below is the block in terraform
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    #for ingress we will allow from_port and to_port and protocol as per our needs
+    ingress {
+        from_port        = var.from_port
+        to_port          = var.to_port
+        protocol         = var.protocol
+        cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "allow_sshh"
+    }
+}
---
+++ variables/variables.tf
@@ -0,0 +1,49 @@
+variable "ami_id" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+    description = "this is the ami_id of devops-practice which is rhel9"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "tags" {
+    type = map #optional
+    default = {
+        Name = "backend"
+        Project = "expense"
+        Component = "backend"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "sg_name" {
+    default = "allow_sshh"
+}
+
+variable "sg_description" {
+    default = "allow port number 22 for ssh access"
+}
+
+variable "from_port" {
+    type = number
+    default = 22
+}
+
+variable "to_port" {
+    type = number
+    default = 22
+}
+
+variable "protocol" {
+    type = string 
+    default = "tcp"
+}
+
+variable "cidr_blocks" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
