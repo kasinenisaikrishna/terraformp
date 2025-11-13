7eba799931c8ce2a3d76eeece397e1c99f22f9c8
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-13 18:25:36 +0530

    terraform

--- count/ec2.tf
+++ count/ec2.tf
@@ -1,9 +1,10 @@
-resource "aws_instance" "terraform"{
+resource "aws_instance" "backend"{
+    count = 3
     ami = "ami-09c813fb71547fc4f"
-    instance_type = var.environment == "prod" ? "t3.small" : "t3.micro"
+    instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = {
-        Name = "terraform"
+        Name = "backend"
     }
 }
 
