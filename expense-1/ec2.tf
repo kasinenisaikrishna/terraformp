8ddea41d72c3959d7991d8c53630649cc35cae7a
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-14 16:27:55 +0530

    terraform

--- expense-1/ec2.tf
+++ expense-1/ec2.tf
@@ -1,36 +1,36 @@
-resource "aws_instance" "terraform"{
-    count = length(var.instance_names)
-    ami = data.aws_ami.ami_info.id
-    instance_type = var.instance_names[count.index] == "mysql" ? "t3.small" : "t3.micro"
-    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-    tags = {
-        Name = var.instance_names[count.index]
-    }
+resource "aws_instance" "terraform" {
+  count                  = length(var.instance_names)
+  ami                    = data.aws_ami.ami_info.id
+  instance_type          = var.instance_names[count.index] == "mysql" ? "t3.small" : "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+  tags = {
+    Name = var.instance_names[count.index]
+  }
 }
 
-resource "aws_security_group" "allow_ssh_terraform"{
-    name = "allow_sshh" #allow_ssh is already there in my account
-    description = "allow port number 22 for ssh access"
-    
-    #usually we allow everything in egress
-    egress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
+resource "aws_security_group" "allow_ssh_terraform" {
+  name        = "allow_sshh" #allow_ssh is already there in my account
+  description = "allow port number 22 for ssh access"
 
-    #for ingress we will allow from_port and to_port and protocol as per our needs
-    ingress {
-        from_port        = 22
-        to_port          = 22
-        protocol         = "tcp"
-        cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
-        ipv6_cidr_blocks = ["::/0"]
-    }
+  #usually we allow everything in egress
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-    tags = {
-        Name = "allow_sshh"
-    }
+  #for ingress we will allow from_port and to_port and protocol as per our needs
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_sshh"
+  }
 }
--- expense-1/variables.tf
+++ expense-1/variables.tf
@@ -1,12 +1,12 @@
-variable "instance_names"{
-    type = list(string)
-    default = ["mysql","backend","frontend"]
+variable "instance_names" {
+  type    = list(string)
+  default = ["mysql", "backend", "frontend"]
 }
 
 variable "zone_id" {
-    default = "Z033712422B61CO3J7OXS" #we get this from route53 hosted zones and dawsconnect hosted zone details and hosted zone id
+  default = "Z033712422B61CO3J7OXS" #we get this from route53 hosted zones and dawsconnect hosted zone details and hosted zone id
 }
 
-variable "domain_name"{
-    default = "dawsconnect.org"
+variable "domain_name" {
+  default = "dawsconnect.org"
 }
